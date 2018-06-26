1060f42ed204a69ff0e6826fb0e7c2e7e4a4982d
Patricia Duduzile <patricia.deemasango@gmail.com>
2018-06-26 16:16:02 -0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,54 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  value = 0;
+  value1 = 0;
+
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  
+  click3() {
+
+  this.value +=3;
 }
+
+click2(){
+
+  this.value +=2;
+  
+}
+
+click1(){
+
+  this.value +=1;
+}
+
+
+ 
+click33(){
+
+  this.value1 +=3;
+}
+
+click22(){
+
+  this.value1 +=2;
+}
+
+click11(){
+
+  this.value1 +=1;
+}
+
+
+Resert()
+{
+  this.value = 0;
+  this.value1 = 0;
+}
+
+}
+
